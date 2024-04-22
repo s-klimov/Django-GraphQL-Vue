257bd66471e7feeec78377fb58f8da3b75ddfe54
Sergey Klimov <ksiu@pkzdrav.ru>
2024-04-22 12:46:04 +0700

    corsheaders added

--- back_end/backend/settings.py
+++ back_end/backend/settings.py
@@ -34,9 +34,11 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
     "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -117,3 +119,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 GRAPHENE = {
     "SCHEMA": "blog.schema.schema",
 }
+
+CORS_ALLOWED_ORIGINS = ("http://localhost:5173",)
