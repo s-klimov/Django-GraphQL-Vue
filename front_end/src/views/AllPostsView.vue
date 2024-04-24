fb78699b0afc289aa6ed67b0178e3e1fe4914f50
Sergey Klimov <ksiu@pkzdrav.ru>
2024-04-24 12:36:55 +0700

    Vue Apollo added

--- front_end/src/views/AllPostsView.vue
+++ front_end/src/views/AllPostsView.vue
@@ -1,14 +1,23 @@
 <script setup>
 import PostList from "../components/PostList.vue";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+const { result, loading, error } = useQuery(gql`
+  query {
+    allPosts {
+      title
+      slug
+      author {
+        user {
+          username
+          firstName
+          lastName
+        }
+      }
+    }
+  }
+`);
 </script>
 
-<template>
-  <h2>Recent Posts</h2>
-  <PostList />
-</template>
-
-<style scoped>
-  h2 {
-    color: blue;
-  }
-</style>
+<!-- ... -->
--- front_end/src/views/PostsByTagView.vue
+++ front_end/src/views/PostsByTagView.vue
@@ -1,14 +1,28 @@
 <script setup>
 import PostList from "../components/PostList.vue";
+import { useRoute } from "vue-router";
+import { useQuery } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+
+const route = useRoute();
+const tag = route.params.tag;
+const { result, loading, error } = useQuery(gql`
+  query {
+    postsByTag(
+      tag: "${tag}"
+    ) {
+        title
+        slug
+        author {
+          user {
+            username
+            firstName
+            lastName
+          }
+        }
+      }
+    }
+`);
 </script>
 
-<template>
-  <h2>Posts by Tag</h2>
-  <PostList />
-</template>
-
-<style scoped>
-  h2 {
-    color: green;
-  }
-</style>
+<!-- ... -->
